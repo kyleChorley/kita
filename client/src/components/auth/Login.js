a8ed005defb2b6ca7a5be849f1bec07d80b2a2e0
Michael Garnreiter <mgarnreiter@aol.com>
2020-03-09 15:58:37 +0100

    adds login box

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -49,32 +49,34 @@ class Login extends Component {
   render() {
     return (
       <div className="flex flex-container center col">
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit} className="flex center col">
-          {/* <label>Username:</label> */}
-          <input
-            placeholder="Username"
-            className="center"
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={e => this.handleChange(e)}
-          />
-          {/* <label>Password:</label> */}
-          <input
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={e => this.handleChange(e)}
-          />
+        <div className="box">
+          <h1>Login</h1>
+          <form onSubmit={this.handleSubmit} className="flex center col">
+            {/* <label>Username:</label> */}
+            <input
+              placeholder="Username"
+              className="center"
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={e => this.handleChange(e)}
+            />
+            {/* <label>Password:</label> */}
+            <input
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={e => this.handleChange(e)}
+            />
 
-          <input type="submit" value="Login" />
-        </form>
-        <p>
-          Don't have account?
-          <Link to={"/signup"}> Signup</Link>
-        </p>
+            <input type="submit" value="Login" />
+          </form>
+          <p>
+            Don't have account?
+            <Link to={"/signup"}> Signup</Link>
+          </p>
+        </div>
       </div>
     );
   }
